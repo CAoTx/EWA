76a1c58528d597af591fd2fb039c2ca5e880b91f
intx <intx@intxs.local>
2018-05-08 15:57:26 +0200

    introduced sim_db

---
+++ client-deliverer/delivererView.js
@@ -0,0 +1,6 @@
+"use strict "
+
+function _getPrice(idx){
+    console.log("Id = " + getPrice(idx).key);
+    console.log("Price = " + getName(idx).value);
+}
---
+++ db.js
@@ -0,0 +1,56 @@
+"use strict "
+
+var PizzaNames = [
+
+    {
+        key: 1, value: "Salami"
+    },
+    {
+        key: 2, value: "Cheese"
+    },
+    {
+        key: 3, value: "Peperoni"
+    },
+    {
+        key: 4, value: "Tuna"
+    },
+    {
+        key: 5, value: "Ham"
+    },
+    {
+        key: 6, value: "Veggi"
+    },
+]
+
+var PizzaPrices = [
+
+    {
+        key: 1, value: 6.00
+    },
+    {
+        key: 2, value: 5.00
+    },
+    {
+        key: 3, value: 6.90
+    },
+    {
+        key: 4, value: 6.00
+    },
+    {
+        key: 5, value: 6.00
+    },
+    {
+        key: 6, value: 6.50
+    },
+]
+
+
+function getPrice(index) {
+
+   return PizzaPrices[index];
+
+}
+
+function getName(index){
+    return PizzaPrices[index];
+}
