ae0c679d731f62a9855324953f091739ba7d89ca
Michal Lipinski <michoo95@hotmail.com>
2018-06-19 05:40:16 +0200

    neue Datenbank

---
+++ sql/DB_Abgabe.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2018 at 05:39 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ewa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_pizza` int(11) NOT NULL,
+  `name_pizza` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `price_pizza` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id_pizza`, `name_pizza`, `price_pizza`) VALUES
+(1, 'Margharita', 5),
+(2, 'Salami', 6),
+(3, 'Pepperoni', 6.37),
+(4, 'Tuna', 6.19),
+(5, 'Quattro Fromaggi', 6.56),
+(6, 'Veggi', 6.42);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered_pizza`
+--
+
+CREATE TABLE `ordered_pizza` (
+  `id_orderedpizza` int(11) NOT NULL,
+  `id_bestellung` int(11) NOT NULL,
+  `name_pizza` varchar(15) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ordered_pizza`
+--
+
+INSERT INTO `ordered_pizza` (`id_orderedpizza`, `id_bestellung`, `name_pizza`, `status`) VALUES
+(1, 9, 'Margharita', 5),
+(12, 10, 'Margharita', 5),
+(13, 10, 'Margharita', 5),
+(14, 10, 'Salami', 5),
+(15, 10, 'Salami', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(11) NOT NULL,
+  `adress_order` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id_order`, `adress_order`) VALUES
+(1, 'Musterstr 1'),
+(10, 'adresse1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_pizza`);
+
+--
+-- Indexes for table `ordered_pizza`
+--
+ALTER TABLE `ordered_pizza`
+  ADD PRIMARY KEY (`id_orderedpizza`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_pizza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `ordered_pizza`
+--
+ALTER TABLE `ordered_pizza`
+  MODIFY `id_orderedpizza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
